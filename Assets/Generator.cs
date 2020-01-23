773d41eba4b55e115a9a4de78c7b856a27eddcc9
Jesper <jeppenp12@gmail.com>
2020-01-23 13:23:38 +0100

    Init + Generator Progress

---
+++ Assets/Generator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+    public float generator_progress;
+
+	// Use this for initialization
+	void Start () {
+		generator_progress = 0f;
+
+		if(generator_progress != 100f) {
+     		InvokeRepeating("ProgressGenerator", 0f, 1f);  //1s delay, repeat every 1s
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Speed
+		
+		
+		if(generator_progress == 100f) {        
+			Debug.Log("Complete");
+
+		}
+	}
+	void ProgressGenerator() {
+		if(generator_progress != 100f) {
+			generator_progress++;
+		}
+ 	}
+}
