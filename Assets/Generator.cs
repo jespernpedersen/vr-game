edeb664c9dc5c87cd53f55373f367154b126799c
Jesper <jeppenp12@gmail.com>
2020-01-23 13:24:21 +0100

    Cleaned up code of generator.cs

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -7,26 +7,22 @@ public class Generator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		generator_progress = 0f;
+		progress = 0f;
 
-		if(generator_progress != 100f) {
+		if(progress != 100f) {
      		InvokeRepeating("ProgressGenerator", 0f, 1f);  //1s delay, repeat every 1s
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Speed
-		
-		
-		if(generator_progress == 100f) {        
+		if(progress == 100f) {        
 			Debug.Log("Complete");
-
 		}
 	}
 	void ProgressGenerator() {
-		if(generator_progress != 100f) {
-			generator_progress++;
+		if(progress != 100f) {
+			progress++;
 		}
  	}
 }
